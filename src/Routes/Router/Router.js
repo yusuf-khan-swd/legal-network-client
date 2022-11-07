ac58b8b49c373f36bf62f1251740bf541031efaf
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-11-08 00:21:25 +0600

    added blog component to others page

--- src/Routes/Router/Router.js
+++ src/Routes/Router/Router.js
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../Layout/Main";
-import Blog from "../../Pages/Blog/Blog";
+import Blog from "../../Pages/Others/Blog/Blog";
 import Home from "../../Pages/Home/Home/Home";
 
 export const routes = createBrowserRouter([
