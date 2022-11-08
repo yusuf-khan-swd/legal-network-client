1e6c5c84f3c4c226111fef85bbe6a69fddabc9cb
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-11-08 22:39:08 +0600

    create a dynamic route for details to load

--- src/Pages/Shared/ServiceCard/ServiceCard.js
+++ src/Pages/Shared/ServiceCard/ServiceCard.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const ServiceCard = ({ service }) => {
-  const { photoURL, name, description } = service;
+  const { photoURL, name, description, _id } = service;
   return (
     <div className="card card-compact w-96 bg-base-100 shadow-xl mx-auto">
       <figure><img src={photoURL} className="h-72" alt={name} /></figure>
       <div className="card-body">
         <h2 className="card-title">{name}</h2>
-        <p>{description}</p>
+        <p>{description.slice(0, 100) + '...'}</p>
         <div className="card-actions justify-end">
-          <button className="btn bg-orange-50 text-black border border-orange-500 hover:bg-orange-100 hover:border-orange-400">view details</button>
+          <Link to={`/services/${_id}`}>
+            <button className="btn bg-orange-50 text-black border border-orange-500 hover:bg-orange-100 hover:border-orange-400">view details</button>
+          </Link>
         </div>
       </div>
     </div>
