a5b078bedd0c7c8c867cd566e1f523deda4de9d5
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-11-08 23:41:09 +0600

    added some padding to card to stand out

--- src/Pages/Service/ServiceDetails/ServiceDetails.js
+++ src/Pages/Service/ServiceDetails/ServiceDetails.js
@@ -5,12 +5,18 @@ const ServiceDetails = () => {
   const { photoURL, name, description, _id } = useLoaderData();
 
   return (
-    <div className="card card-compact w-96 bg-base-100 shadow-xl mx-auto">
-      <figure><img src={photoURL} className="h-72" alt={name} /></figure>
-      <div className="card-body">
-        <h2 className="card-title">{name}</h2>
-        <p>{description}</p>
+    <div>
+      <h2 className='text-3xl font-bold'>Service Section</h2>
+      <div className='max-w-screen-md border px-4 py-16 md:px-24 lg:px-8 lg:py-20 rounded-md mx-auto m-5'>
+        <div className="card card-compact w-96 bg-base-100 shadow-xl mx-auto">
+          <figure><img src={photoURL} className="h-72" alt={name} /></figure>
+          <div className="card-body">
+            <h2 className="card-title">{name}</h2>
+            <p>{description}</p>
+          </div>
+        </div>
       </div>
+
     </div>
   );
 };
