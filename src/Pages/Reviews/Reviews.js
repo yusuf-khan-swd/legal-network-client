2dc01e51632d3e1e8c0fab675984c914aa01fd4c
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-11-08 13:59:36 +0600

    logout the user if token or user is not valid

--- src/Pages/Reviews/Reviews.js
+++ src/Pages/Reviews/Reviews.js
@@ -10,11 +10,16 @@ const Reviews = () => {
         authorization: `Bearer ${localStorage.getItem('legal-token')}`
       }
     })
-      .then(res => res.json())
+      .then(res => {
+        if (res.status === 401 || res.status === 403) {
+          return logOut();
+        }
+        return res.json();
+      })
       .then(data => {
         console.log(data);
       })
-  }, [user?.email]);
+  }, [user?.email, logOut]);
   return (
     <div>
       <h2>Reviews</h2>
