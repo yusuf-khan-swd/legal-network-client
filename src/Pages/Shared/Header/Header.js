3145705260162841cfede46c95c43e9bce447465
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-11-08 17:13:01 +0600

    added conditional rendering to small device

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -27,11 +27,31 @@ const Header = () => {
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
               </label>
               <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                <li><a>Item 1</a></li>
-                <li><a>Item 3</a></li>
+                <li>
+                  <Link to='/home'>Home</Link>
+                </li>
+                {
+                  user?.uid ?
+                    <>
+                      <li>
+                        <Link to='/reviews'>My Reviews</Link>
+                      </li>
+                      <li>
+                        <Link>Add Service</Link>
+                      </li>
+                      <li><button onClick={handleLogOut}>Logout</button></li>
+                    </>
+                    :
+                    <>
+                      <Link to='/login'>Login</Link>
+                    </>
+                }
+                <li className='border-2 border-orange-400 rounded-lg'>
+                  <Link to='/blog'>Blog</Link>
+                </li>
               </ul>
             </div>
-            <a className="btn btn-ghost normal-case text-xl">Legal Network</a>
+            <Link to='/' className="btn btn-ghost normal-case text-xl">Legal Network</Link>
           </div>
           <div className="navbar-center hidden lg:flex">
             <ul className="menu menu-horizontal p-0">
@@ -63,7 +83,7 @@ const Header = () => {
 
             <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
               <div className="w-10 rounded-full">
-                <img src="https://placeimg.com/80/80/people" />
+                <img src={user?.photoURL} alt={user?.displayName} />
               </div>
             </label>
 
