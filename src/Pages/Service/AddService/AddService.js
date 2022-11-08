ebc5a66653a61ca546c02f260a9bc7f03df3db40
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-11-08 20:03:46 +0600

    service send to server

--- src/Pages/Service/AddService/AddService.js
+++ src/Pages/Service/AddService/AddService.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import { useForm } from 'react-hook-form';
 import { AuthContext } from '../../../contexts/AuthProvider/AuthProvider';
 import useTitle from '../../../hooks/useTitle';
@@ -11,6 +11,18 @@ const AddService = () => {
 
   const onSubmit = value => {
     console.log(value);
+
+    fetch('http://localhost:5000/services', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(value)
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+      })
   };
 
   return (
@@ -18,7 +30,7 @@ const AddService = () => {
       <div className="hero-content">
         <div className="card flex-shrink-0 w-full max-w-screen-sm shadow-2xl bg-base-100">
           <form onSubmit={handleSubmit(onSubmit)} className="card-body">
-            <h2 className='text-3xl font-bold text-center hover:underline cursor-pointer text-orange-500'>Add Service</h2>
+            <h2 className='text-3xl font-bold text-center hover:underline cursor-pointer text-orange-500'>Create a Service</h2>
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Service Name</span>
@@ -48,15 +60,13 @@ const AddService = () => {
               <input type="email" {...register('email')} defaultValue={user?.email} readOnly className="input input-bordered" />
             </div>
             <div className="form-control">
-              <textarea {...register('description', { minLength: 110 })} className="textarea textarea-bordered" cols="30" rows="10" placeholder="Description"></textarea>
+              <textarea {...register('description', { minLength: 120 })} className="textarea textarea-bordered" cols="30" rows="5" placeholder="Description"></textarea>
               {errors.description &&
                 <label className="label">
-                  <span className="label-text text-red-500">Description must be minimum greater then 110 character</span>
+                  <span className="label-text text-red-500">Description must be greater then 120 character</span>
                 </label>
               }
-
             </div>
-
             <div className="form-control mt-6">
               <button type='submit' className="btn bg-orange-50 border border-orange-400 text-orange-400 hover:bg-orange-200 hover:border-orange-600">Add Service</button>
             </div>
