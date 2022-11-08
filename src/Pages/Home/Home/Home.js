6ae169a26d04b1cc6d22847b7588e128fc672f3e
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-11-08 22:22:00 +0600

    added gap to card

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
   return (
     <div className='container mx-auto my-8'>
       <h2>Home Component.</h2>
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
         {
           popularServices.map(service => <ServiceCard key={service._id} service={service}></ServiceCard>)
         }
--- src/Pages/Service/Services/Services.js
+++ src/Pages/Service/Services/Services.js
@@ -8,7 +8,7 @@ const Services = () => {
   return (
     <div className='container mx-auto'>
       <h2>All Services</h2>
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
         {
           services.map(service => <ServiceCard key={service._id} service={service}></ServiceCard>)
         }
