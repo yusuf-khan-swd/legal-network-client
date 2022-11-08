4071b141712b2c04bc26338ed0c184c9f1558bbd
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-11-08 17:09:37 +0600

    added navbar

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -19,21 +19,57 @@ const Header = () => {
 
   return (
     <div>
-      <h2>Header</h2>
-      <Link to='/home'>Home</Link>
-      <Link to='/blog'>Blog</Link>
-      {
-        user?.uid ?
-          <>
-            <Link to='/reviews'>My Reviews</Link>
-            <Link>Add Service</Link>
-            <button onClick={handleLogOut}>Logout</button>
-          </>
-          :
-          <>
-            <Link to='/login'>Login</Link>
-          </>
-      }
+      <nav>
+        <div className="navbar bg-base-100">
+          <div className="navbar-start">
+            <div className="dropdown">
+              <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+              </label>
+              <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                <li><a>Item 1</a></li>
+                <li><a>Item 3</a></li>
+              </ul>
+            </div>
+            <a className="btn btn-ghost normal-case text-xl">Legal Network</a>
+          </div>
+          <div className="navbar-center hidden lg:flex">
+            <ul className="menu menu-horizontal p-0">
+              <li>
+                <Link to='/home'>Home</Link>
+              </li>
+              {
+                user?.uid ?
+                  <>
+                    <li>
+                      <Link to='/reviews'>My Reviews</Link>
+                    </li>
+                    <li>
+                      <Link>Add Service</Link>
+                    </li>
+                    <li><button onClick={handleLogOut}>Logout</button></li>
+                  </>
+                  :
+                  <>
+                    <Link to='/login'>Login</Link>
+                  </>
+              }
+              <li className='border-2 border-orange-400 rounded-lg'>
+                <Link to='/blog'>Blog</Link>
+              </li>
+            </ul>
+          </div>
+          <div className="navbar-end">
+
+            <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+              <div className="w-10 rounded-full">
+                <img src="https://placeimg.com/80/80/people" />
+              </div>
+            </label>
+
+          </div>
+        </div>
+      </nav>
     </div>
   );
 };
