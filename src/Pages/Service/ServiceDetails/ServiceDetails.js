06f0589d6bf4a7b0f44b65470d5538f07e801e63
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-11-08 23:31:40 +0600

    remove btn

--- src/Pages/Service/ServiceDetails/ServiceDetails.js
+++ src/Pages/Service/ServiceDetails/ServiceDetails.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, useLoaderData } from 'react-router-dom';
+import { useLoaderData } from 'react-router-dom';
 
 const ServiceDetails = () => {
   const { photoURL, name, description, _id } = useLoaderData();
@@ -10,9 +10,6 @@ const ServiceDetails = () => {
       <div className="card-body">
         <h2 className="card-title">{name}</h2>
         <p>{description}</p>
-        <div className="card-actions justify-end">
-          <button className="px-4 py-2 bg-orange-50 text-black border border-orange-500 hover:bg-orange-100 hover:border-orange-400">Delete</button>
-        </div>
       </div>
     </div>
   );
