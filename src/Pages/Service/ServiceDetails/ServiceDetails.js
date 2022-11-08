ccde4088b250b20a093047c54fa719b9360bf582
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-11-08 22:55:44 +0600

    service details page added

--- src/Pages/Service/ServiceDetails/ServiceDetails.js
+++ src/Pages/Service/ServiceDetails/ServiceDetails.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { Link, useLoaderData } from 'react-router-dom';
 
 const ServiceDetails = () => {
-  const service = useLoaderData();
-  console.log(service);
+  const { photoURL, name, description, _id } = useLoaderData();
 
   return (
-    <div>
-      <h2>Service Card</h2>
+    <div className="card card-compact w-96 bg-base-100 shadow-xl mx-auto">
+      <figure><img src={photoURL} className="h-72" alt={name} /></figure>
+      <div className="card-body">
+        <h2 className="card-title">{name}</h2>
+        <p>{description}</p>
+        <div className="card-actions justify-end">
+          <button className="px-4 py-2 bg-orange-50 text-black border border-orange-500 hover:bg-orange-100 hover:border-orange-400">Delete</button>
+        </div>
+      </div>
     </div>
   );
 };
