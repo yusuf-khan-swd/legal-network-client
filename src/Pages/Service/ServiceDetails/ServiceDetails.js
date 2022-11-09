552ef3ffb02657eeadb865dc4b31d3b1258567b5
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-11-09 23:28:08 +0600

    add hr

--- src/Pages/Service/ServiceDetails/ServiceDetails.js
+++ src/Pages/Service/ServiceDetails/ServiceDetails.js
@@ -48,7 +48,6 @@ const ServiceDetails = () => {
 
   return (
     <div>
-      <h2 className='text-3xl font-bold'>Service Section</h2>
       <div className='max-w-screen-md border px-4 py-16 md:px-24 lg:px-8 lg:py-20 rounded-md mx-auto m-5'>
         <div className="card card-compact w-96 lg:w-3/4 bg-base-100 shadow-xl mx-auto">
           <figure><img src={photoURL} className="h-72 rounded-full" alt={name} /></figure>
@@ -60,7 +59,7 @@ const ServiceDetails = () => {
         </div>
       </div>
       <div>
-        <h2 className='text-3xl font-bold'>Review Section</h2>
+        <hr className='w-11/12 mx-auto' />
         {
           user?.uid ?
             <div>
@@ -102,7 +101,7 @@ const ServiceDetails = () => {
             </div>
         }
         <div>
-          <h2 className='text-xl font-bold'>Show all review</h2>
+
           <div className='grid grid-cols-1 gap-8'>
             {
               reviews.map(review => <ReviewCard key={review._id} review={review}></ReviewCard>)
