0acdecf8485a76d4f7395af2b07c4d373b6079be
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-11-10 16:08:51 +0600

    added banner component and heading to home

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -7,6 +7,8 @@ import Team from '../Team/Team';
 import 'react-photo-view/dist/react-photo-view.css';
 import { PhotoProvider } from 'react-photo-view';
 import useTitle from '../../../hooks/useTitle';
+import Banner from '../Banner/Banner';
+import { FaArrowDown } from 'react-icons/fa';
 
 const Home = () => {
   const popularServices = useLoaderData();
@@ -14,6 +16,11 @@ const Home = () => {
 
   return (
     <div className='container mx-auto my-8'>
+      <Banner></Banner>
+      <div>
+        <h2 className='text-2xl underline underline-offset-8 font-bold text-orange-400 text-center mb-3'>The Popular Services That We Provide</h2>
+        <p> <FaArrowDown className='text-3xl text-orange-400 mx-auto'></FaArrowDown> </p>
+      </div>
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
         <PhotoProvider>
           {
