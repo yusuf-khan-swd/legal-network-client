014430a5ca8707a63b7d4156369f6d68c760d75c
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-11-10 22:06:04 +0600

    some modification are added in footer

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -7,45 +7,44 @@ const Footer = () => {
       <footer className="p-6 dark:bg-gray-800 dark:text-gray-100">
         <div className="container grid grid-cols-2 mx-auto gap-x-3 gap-y-8 sm:grid-cols-3 md:grid-cols-4">
           <div className="flex flex-col space-y-4">
-            <h2 className="font-medium">Getting started</h2>
+            <h2 className="font-medium">Need to Know About Us</h2>
             <div className="flex flex-col space-y-2 text-sm dark:text-gray-400">
-              <a className="link link-hover" href="#">Installation</a>
-              <a className="link link-hover" href="#">Release Notes</a>
-              <a className="link link-hover" href="#">Upgrade Guide</a>
-              <a className="link link-hover" href="#">Using with Preprocessors</a>
-              <a className="link link-hover" href="#">Optimizing for Production</a>
-              <a className="link link-hover" href="#">Browser Support</a>
-              <a className="link link-hover" href="#">IntelliSense</a>
+              <Link className="link link-hover" to='/services'>Services</Link>
+              <Link className="link link-hover" to='/'>Release Notes</Link>
+              <Link className="link link-hover" to='/'>Upgrade Guide</Link>
+              <Link className="link link-hover" to='/'>Super Cool Stuff</Link>
+              <Link className="link link-hover" to='/'>Optimizing for People</Link>
+              <Link className="link link-hover" to='/'>All Time Support</Link>
+              <Link className="link link-hover" to='/'>IntelliSense Provide</Link>
             </div>
           </div>
           <div className="flex flex-col space-y-4">
             <h2 className="font-medium">Core Concepts</h2>
             <div className="flex flex-col space-y-2 text-sm dark:text-gray-400">
-              <a className="link link-hover" href="#">Utility-First</a>
-              <a className="link link-hover" href="#">Responsive Design</a>
-              <a className="link link-hover" href="#">Hover, Focus, &amp; Other States</a>
-              <a className="link link-hover" href="#">Dark Mode</a>
-              <a className="link link-hover" href="#">Adding Base Styles</a>
-              <a className="link link-hover" href="#">Extracting Components</a>
-              <a className="link link-hover" href="#">Adding New Utilities</a>
+              <Link className="link link-hover" to='/'>Law is first</Link>
+              <Link className="link link-hover" to='/'>Responsible Answer</Link>
+              <Link className="link link-hover" to='/'>Dark Mode</Link>
+              <Link className="link link-hover" to='/'>Adding Base Styles</Link>
+              <Link className="link link-hover" to='/'>Extracting Components</Link>
+              <Link className="link link-hover" to='/'>Adding New Utilities</Link>
             </div>
           </div>
           <div className="flex flex-col space-y-4">
             <h2 className="font-medium">COMPANY</h2>
             <div className="flex flex-col space-y-2 text-sm dark:text-gray-400">
-              <a className="link link-hover">About us</a>
-              <a className="link link-hover">Contact</a>
-              <a className="link link-hover">Jobs</a>
-              <a className="link link-hover">Press kit</a>
+              <Link className="link link-hover" to='/'>About us</Link>
+              <Link className="link link-hover" to='/'>Contact</Link>
+              <Link className="link link-hover" to='/'>Jobs</Link>
+              <Link className="link link-hover" to='/'>Press kit</Link>
             </div>
           </div>
           <div className="flex flex-col space-y-4">
             <h2 className="font-medium">Community</h2>
             <div className="flex flex-col space-y-2 text-sm dark:text-gray-400">
-              <a className="link link-hover" href="#">GitHub</a>
-              <a className="link link-hover" href="#">Discord</a>
-              <a className="link link-hover" href="#">Twitter</a>
-              <a className="link link-hover" href="#">YouTube</a>
+              <Link className="link link-hover" to='/'>GitHub</Link>
+              <Link className="link link-hover" to='/'>Discord</Link>
+              <Link className="link link-hover" to='/'>Twitter</Link>
+              <Link className="link link-hover" to='/'>YouTube</Link>
             </div>
           </div>
         </div>
