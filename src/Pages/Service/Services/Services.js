cc5c7a505b10c97efa6824ee300dd1934a788417
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-11-10 21:46:12 +0600

    modified some style

--- src/Pages/Service/Services/Services.js
+++ src/Pages/Service/Services/Services.js
@@ -32,7 +32,7 @@ const Services = () => {
 
   return (
     <div className='container mx-auto my-8'>
-      <h2 className='text-3xl font-bold text-center text-orange-400 hover:underline cursor-pointer mb-3'>All Services</h2>
+      <h2 className='text-3xl font-bold text-center text-orange-400 underline cursor-pointer mb-3'>All Services</h2>
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
         <PhotoProvider>
           {
